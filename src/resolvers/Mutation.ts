6204aa99705e71cb62ef7eda9e89365866b7d644
Osamah Aldoaiss <osamah@aldoaiss.de>
2018-12-28 16:51:52 +0100

    add signup changes

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -5,31 +5,56 @@ import { APP_SECRET } from '../utils';
 
 export const Mutation: MutationResolvers.Type = {
   ...MutationResolvers.defaultResolvers,
+
   signup: async (_: any, { firstName, lastName, email, password, projectName }, context: any) => {
-    const project = await context.prisma.project({ name: projectName });
-    console.log(project);
-    if (!project) throw new Error('Project not found');
+    let project = await context.prisma.project({ name: projectName });
+    if (!project) {
+      try {
+        project = context.prisma.createProject({ name: projectName });
+      } catch (error) {
+        throw new Error('E0003: Project could not be created!');
+      }
+    }
+
+    const user = await context.prisma.user({ email });
+    if (user) throw new Error('E0004: A user with this email already exists!');
+
     const hashedPassword = await hash(password, 10);
-    const user = await context.prisma.createUser({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    });
-    return {
-      token: sign({ userId: user.id }, APP_SECRET),
-      user,
-    };
+    try {
+      const newUser = await context.prisma.createUser({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        projects: [
+          {
+            connect: {
+              name: project.name,
+            },
+          },
+        ],
+      });
+
+      return {
+        token: sign({ userId: user.id }, APP_SECRET),
+        user: newUser,
+      };
+    } catch (error) {
+      throw new Error('E0005: User could not be created' + error.message);
+    }
   },
+
   login: async (parent: any, { email, password }, context: any) => {
     const user = await context.prisma.user({ email });
     if (!user) {
-      throw new Error(`No user found for email: ${email}`);
+      throw new Error(`E0001: No user found for email: ${email}`);
     }
+
     const passwordValid = await compare(password, user.password);
     if (!passwordValid) {
-      throw new Error('Invalid password');
+      throw new Error('E0001: Invalid password');
     }
+
     return {
       token: sign({ userId: user.id }, APP_SECRET),
       user,
